a96f35646f04398ce39e7363485be67656742c0a
Vianney Nguyen <nguyenvianney@gmail.com>
2017-08-22 10:32:20 -0600

    Adjust HealthObject to call methods in subclass
    HealthObject will call pure virtual methods during update to call
    object-specific updates.

--- Classes/HealthObject.cpp
+++ Classes/HealthObject.cpp
@@ -10,7 +10,24 @@ HealthObject::HealthObject(float maxObjectHealth)
 HealthObject::~HealthObject() {}
 
 void HealthObject::update(float delta, Bullet *bullet) {
-    
+    if (!active) return;
+
+    // Deduct the object's health if the object is hit by a bullet
+    if (getBoundingBox().intersectsRect(bullet->getBoundingBox())) {
+
+        // Mark the bullet as inactive to be later disposed by the GameScene
+        bullet->deactivate();
+
+        health -= bullet->getDamage();
+
+        if (health <= 0) {
+            setIsActive(false);
+            onDestroyItem();
+            return;
+        }
+    }
+
+    updateItem(delta);
 }
 
 float HealthObject::getHealth() const {
